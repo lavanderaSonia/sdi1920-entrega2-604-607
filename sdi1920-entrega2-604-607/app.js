f3ab0720857cf8c77e1661346e9342333ac8e2c9
Sonia <UO263536@uniovi.es>
2020-04-27 13:22:35 +0200

    Añadiendo app.js y views

---
+++ sdi1920-entrega2-604-607/app.js
@@ -0,0 +1,131 @@
+//Módulos
+let express = require('express');
+let app = express();
+
+let os = require('os');
+let puerto = 3000;
+
+app.listen(puerto, function () {
+console.log("Servidor listo " + puerto);
+})
+
+let rest = require('request');
+app.set('rest', rest);
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+    // Debemos especificar todas las headers que se aceptan. Content-Type , token
+    next();
+});
+
+var jwt = require('jsonwebtoken');
+app.set('jwt', jwt);
+
+let fs = require('fs');
+let https = require('https');
+
+let expressSession = require('express-session');
+app.use(expressSession({
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: true
+}));
+
+let crypto = require('crypto');
+
+let fileUpload = require('express-fileupload');
+app.use(fileUpload());
+let mongo = require('mongodb');
+let swig = require('swig');
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static('public'));
+
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app, mongo);
+
+// routerUsuarioToken
+var routerUsuarioToken = express.Router();
+routerUsuarioToken.use(function (req, res, next) {
+    // obtener el token, vía headers (opcionalmente GET y/o POST).
+    var token = req.headers['token'] || req.body.token || req.query.token;
+    if (token != null) {
+        // verificar el token
+        jwt.verify(token, 'secreto', function (err, infoToken) {
+            if (err || (Date.now() / 1000 - infoToken.tiempo) > 240) {
+                res.status(403); // Forbidden
+                res.json({
+                    acceso: false,
+                    error: 'Token invalido o caducado'
+                });
+                // También podríamos comprobar que intoToken.usuario existe
+                return;
+            } else {
+                // dejamos correr la petición
+                res.usuario = infoToken.usuario;
+                next();
+            }
+        });
+    } else {
+        res.status(403); // Forbidden
+        res.json({
+            acceso: false,
+            mensaje: 'No hay Token'
+        });
+    }
+});
+// Aplicar routerUsuarioToken
+app.use('/api/cancion', routerUsuarioToken);
+
+// routerUsuarioSession
+let routerUsuarioSession = express.Router();
+routerUsuarioSession.use(function (req, res, next) {
+    console.log("routerUsuarioSession");
+    if (req.session.usuario) { // dejamos correr la petición
+        next();
+    } else {
+        console.log("va a : " + req.session.destino);
+        res.redirect("/identificarse");
+    }
+});
+
+
+
+
+app.use(express.static('public'));
+
+//Variables
+app.set('port', 8081);
+app.set('db', 'mongodb://admin:sdiadmin1920607@tiendamusica-shard-00-00-9fg70.mongodb.net:27017,tiendamusica-shard-00-01-9fg70.mongodb.net:27017,tiendamusica-shard-00-02-9fg70.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true&w=majority');
+app.set('clave', 'abcdefg');
+app.set('crypto', crypto);
+
+
+//Rutas/controladores por lógica
+require("./routes/rusuarios")(app, swig, gestorBD); //(app,param1, param2, etc.)
+
+
+
+
+
+app.use(function (err, req, res, next) {
+    console.log("Error producido: " + err); //mostramos el error en consola
+    if (!res.headersSent) {
+        res.status(400);
+        res.send("Recurso no disponible");
+    }
+});
+
+//lanzar el servidor
+https.createServer({
+    key: fs.readFileSync('certificates/alice.key'),
+    cert: fs.readFileSync('certificates/alice.crt')
+}, app).listen(app.get('port'), function () {
+    console.log("Servidor activo");
+});
