c601b3d284f233bc9525b243fd22c91278be8aaf
Sonia <UO263536@uniovi.es>
2020-05-01 16:24:18 +0200

    Registro usuarios

--- sdi1920-entrega2-604-607/routes/rusuarios.js
+++ sdi1920-entrega2-604-607/routes/rusuarios.js
@@ -13,16 +13,77 @@ module.exports = function (app, swig, gestorBD) {
             .update(req.body.password).digest('hex');
         let usuario = {
             email: req.body.email,
-            password: seguro
-        };
-        gestorBD.insertarUsuario(usuario, function (id) {
-            if (id == null) {
-                res.redirect("/registrarse?mensaje=Error al registrar usuario");
-            } else {
-                res.redirect("/identificarse?mensaje=Nuevo usuario registrado");
+            nombre: req.body.nombre,
+            apellidos: req.body.apellidos,
+            passwordSinEncriptar: req.body.password,
+            password: seguro,
+            repetirPassword: req.body.repetirPassword
+        }
+        console.log(usuario.email)
+        console.log(usuario.passwordSinEncriptar)
+        console.log(usuario.repetirPassword)
+        validarDatosRegistroUsuario(usuario, function (errors) {
+            if(errors!==null && errors.length>0){
+                res.redirect("/registrarse?mensaje=" + errors);
             }
-        });
+            else{
+                let usuarioInsertar = {
+                    email: usuario.email,
+                    password: usuario.password
+                }
+                gestorBD.insertarUsuario(usuarioInsertar, function (id) {
+                    if (id == null) {
+                        res.redirect("/registrarse?mensaje=Error al registrar usuario");
+                    } else {
+                        res.redirect("/identificarse?mensaje=Nuevo usuario registrado");
+                    }
+                });
+            }
+
+        })
+
     });
+
+
+
+    /**
+     * Función que nos permite validar los campos del formulario de registro del usuario
+     * Valida nombre, apellidos, email si están vacíos
+     * Valida si la contraseña coincide con la repetición de esta
+     * Valida si ya existe un usuario con ese email en el sistema
+     * @param usuario datos del usuario que intenta registrarse
+     * @param functionCallback
+     */
+    function validarDatosRegistroUsuario(usuario, functionCallback){
+            let errores = new Array();
+
+            if(usuario.email===null || typeof usuario.email==='undefined' || usuario.email==="")
+                errores.push('email del usuario no puede estar vacío');
+            if(usuario.nombre===null || typeof usuario.nombre==='undefined' || usuario.nombre==="")
+                errores.push('nombre del usuario no puede estar vacío');
+            if(usuario.apellidos===null || typeof usuario.apellidos==='undefined' || usuario.apellidos==="")
+                errores.push('apellidos del usuario no puede estar vacío');
+            if(usuario.password===null || typeof usuario.password==='undefined' || usuario.password==="")
+                errores.push('password del usuario no puede estar vacío');
+            if(usuario.repetirPassword===null || typeof usuario.repetirPassword==='undefined' || usuario.repetirPassword==="")
+                errores.push('password del usuario no puede estar vacío');
+            if(usuario.passwordSinEncriptar!==usuario.repetirPassword)
+                errores.push('repetición de contraseña inválida');
+
+            let criterio = {
+                email: usuario.email,
+                password:usuario.password
+            }
+            gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+            if(usuarios!=null)
+                errores.push('el email ya existe en el sistema');
+            })
+
+        if(errores.length<=0)
+                functionCallback(null);
+            else
+                functionCallback(errores);
+    }
 
     app.get("/identificarse", function (req, res) {
         let respuesta = swig.renderFile('views/bidentificacion.html', {});
